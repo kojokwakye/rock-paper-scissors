7a24ef2399ac58ac047135f25ceebb5c31336d23
kojokwakye <kojokwakye808@gmail.com>
2022-07-03 02:28:47 +0000

    no major changes

--- external.js
+++ external.js
@@ -1,6 +1,14 @@
-let plays = ["rock", "paper", "scissors"];
+const plays = ["rock", "paper", "scissors"];
 
 function computerPlay() {
   return plays[Math.floor(Math.random() * plays.length)];
 }
-// console.log(computerPlay());
+console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+  
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
