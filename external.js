0bf390d45a0b63a687f3a4e4b5d97c0c0c2cdb1e
kojokwakye <kojokwakye808@gmail.com>
2022-07-03 03:06:34 +0000

    first function works

--- external.js
+++ external.js
@@ -1,12 +1,11 @@
-const plays = ["rock", "paper", "scissors"];
-
 function computerPlay() {
+  const plays = ["rock", "paper", "scissors"];
   return plays[Math.floor(Math.random() * plays.length)];
 }
 console.log(computerPlay());
 
 function playRound(playerSelection, computerSelection) {
-  
+  return 'hahaha'
 }
 
 const playerSelection = "rock";
