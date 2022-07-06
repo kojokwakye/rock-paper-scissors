93215cfabfbb874e92e472d311401ebdda78549a
kojokwakye <kojokwakye808@gmail.com>
2022-07-06 03:27:57 +0000

    regexp doesnt make any sense yet

--- external.js
+++ external.js
@@ -7,12 +7,14 @@ function playRound(playerSelection, computerSelection) {
   if (computerSelection === playerSelection) {
     return "its a tie";
   } else if (computerSelection === "paper") {
-    return "computer picked paper. you win";
+    return "computer picked paper. you lose";
   } else {
     return "computer picked scissors. you win!";
   }
 }
-
-const playerSelection = "rock";
+const playerSelection = /rock/gi;
 const computerSelection = computerPlay();
+console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
+
+
