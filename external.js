77b5eb6c845e802158fd55ececa565ea3ffd8f76
kojokwakye <kojokwakye808@gmail.com>
2022-07-07 03:43:22 +0000

    change if/else condition

--- external.js
+++ external.js
@@ -6,15 +6,24 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   if (computerSelection === playerSelection) {
     return "its a tie";
-  } else if (computerSelection === "paper") {
-    return "computer picked paper. you lose";
+  } else if (
+    (computerSelection === "rock" && playerSelection === "paper") ||
+    (computerSelection === "scissors" && playerSelection === "rock") ||
+    (computerSelection === "paper" && playerSelection === "scissors")
+  ) {
+    return "you win";
+  } else if (
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "scissors" && playerSelection === "paper")
+  ) {
+    return "you lose";
   } else {
-    return "computer picked scissors. you win!";
+    alert("pick one item, please");
   }
 }
-const playerSelection = /rock/gi;
+
+const playerSelection = prompt("rock, paper or scissors").toLowerCase();
 const computerSelection = computerPlay();
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
-
-
