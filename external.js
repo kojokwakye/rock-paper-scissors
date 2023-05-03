c6cf2b1e3440a284c6bb338d8ec0b760ce6f5a0e
kojokwakye <kojokwakye808@gmail.com>
2023-05-03 05:10:52 +0000

    computerselection picks at random now

--- external.js
+++ external.js
@@ -1,7 +1,26 @@
+let computerScore = parseInt(0);
+let playerScore = parseInt(0);
+
 function computerPlay() {
   const plays = ["rock", "paper", "scissors"];
   return plays[Math.floor(Math.random() * plays.length)];
 }
+
+const win = "you win";
+const lose = "you lose";
+const tie = "it's a tie";
+
+const computerSelection = computerPlay();
+// let playerSelection = button;
+
+const button = document.querySelectorAll("button");
+button.forEach((button) => {
+  button.addEventListener("click", () => {
+    let playerSelection = button.id;
+    const computerSelection = computerPlay();
+    console.log(playerSelection, computerSelection);
+  });
+});
 
 function playRound(playerSelection, computerSelection) {
   if (computerSelection === playerSelection) {
@@ -23,43 +42,29 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const win = "you win";
-const lose = "you lose";
-const tie = "it's a tie";
-
-function game() {
-  let computerScore = parseInt(0);
-  let playerScore = parseInt(0);
-
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt(
-      "pick a move. rock, paper or scissors:"
-    ).toLowerCase();
-    const computerSelection = computerPlay();
-
-    if (playRound(playerSelection, computerSelection) === tie) {
-      i--;
-      console.log(tie);
-    } else if (playRound(playerSelection, computerSelection) === win) {
-      playerScore++;
-      console.log(win);
-    } else if (playRound(playerSelection, computerSelection) === lose) {
-      computerScore++;
-      console.log(lose);
-    } else {
-      alert("pick an item");
-    }
-    console.log(
-      "you picked " + playerSelection,
-      "but",
-      "computer picked " + computerSelection
-    );
-  }
-  console.log("you got " + playerScore, "& computer got " + computerScore);
-  if (playerScore >= 3) {
-    console.log("you won this round");
-  } else {
-    console.log("you lose. play another around");
-  }
-}
-game();
+// function game() {
+//   if (playRound(playerSelection, computerSelection === tie)) {
+//     i--;
+//     console.log(tie);
+//   } else if (playRound(playerSelection, computerSelection === win)) {
+//     playerScore++;
+//     console.log(win);
+//   } else if (playRound(playerSelection, computerSelection === lose)) {
+//     computerScore++;
+//     console.log(lose);
+//   } else {
+//     alert("pick an item");
+//   }
+//   console.log(
+//     "you picked " + playerSelection,
+//     "but",
+//     "computer picked " + computerSelection
+//   );
+// }
+// console.log("you got " + playerScore, "& computer got " + computerScore);
+// if (playerScore >= 3) {
+//   console.log("you won this round");
+// } else {
+//   console.log("you lose. play another around");
+// }
+// game(playerSelection, computerSelection);
